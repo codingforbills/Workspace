723a0d169069787ae41e9a7ddab7e6a3e8d2d032
Robert Bills <55222340+CygnusXLR@users.noreply.github.com>
2023-05-31 14:46:01 -0700

    Added main javascript file

---
+++ Main.js
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main (String [] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Player 1, what is your name?");
+        String p1 = in.nextLine();
+
+        System.out.println("Player 2, what is your name?");
+        String p2 = in.nextLine();
+
+        //3x3 Tic Tac Toe board
+        //- empty space
+        //x player 1
+        //o player 2
+
+        char [][] board = new char[3][3];
+
+        //Fill the board with dashes
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                board [i] [j] = '-';
+            }
+        }
+        
+        //draw the board
+    }
+
+} 
