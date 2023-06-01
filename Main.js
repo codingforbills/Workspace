4836ffb560195ae105b5295c8c3d7f4056809339
Robert Bills <55222340+CygnusXLR@users.noreply.github.com>
2023-05-31 19:41:00 -0700

    Updated commit changes

--- Main.js
+++ Main.js
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-class Main {
+class Main { 
     public static void main (String [] args) {
 
         Scanner in = new Scanner(System.in);
 
+        //Get the player's names
         System.out.println("Player 1, what is your name?");
         String p1 = in.nextLine();
-
         System.out.println("Player 2, what is your name?");
         String p2 = in.nextLine();
 
@@ -15,18 +15,21 @@ class Main {
         //- empty space
         //x player 1
         //o player 2
-
         char [][] board = new char[3][3];
 
         //Fill the board with dashes
-
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 board [i] [j] = '-';
             }
-        }
-        
-        //draw the board
+        }  
+    
     }
-
-} 
+    public static void drawBoard (char [] [] board) {
+        for(int i =0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.println(board[i][j]);
+            }
+        }
+    }
+}
